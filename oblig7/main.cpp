89699bd123d507577893c9be6578cceec322cda0
Einar <einar.aglen99@gmail.com>
2021-10-11 21:13:25 +0200

    Oblig7

---
+++ oblig7/main.cpp
@@ -0,0 +1,50 @@
+#include "fraction.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void print(const string &text, const Fraction &broek) {
+  cout << text << broek.numerator << " / " << broek.denominator << endl;
+}
+
+int main() {
+  Fraction a(10, 20);
+  Fraction b(3, 4);
+  Fraction c;
+  c.set(5);
+  Fraction d = a / b;
+
+  print("a = ", a);
+  print("b = ", b);
+  print("c = ", c);
+  print("d = ", d);
+
+  b += a;
+  ++c;
+  d *= d;
+
+  print("b = ", b);
+  print("c = ", c);
+  print("d = ", d);
+
+  c = a + b - d * a;
+  c = -c;
+
+  print("c = ", c);
+
+  if (a + b != c + d)
+    cout << "a + b != c + d" << endl;
+  else
+    cout << " a + b == c + d" << endl;
+  while (b > a)
+    b -= a;
+  print("b = ", b);
+  /** OPPGAVE 1 TEST **/
+  Fraction fraction(1, 2);
+  Fraction result1 = fraction - 5;
+  //Fraction result2 =  1 - fraction;
+  print("result1 = ", result1);
+  //print("result2 = ", result2);
+  
+}
